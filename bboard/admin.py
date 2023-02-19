8f365c5eaaa63ca552f78e88a94eb95ae546b95b
Schtirlitz80 <schtirlitz80@mail.ru>
2023-02-19 21:30:02 +0300

    Rubrics added

--- bboard/admin.py
+++ bboard/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 
-from .models import Bb
+from .models import Bb, Rubric
 
 
 class BdAdmin(admin.ModelAdmin):
-    list_display = ('title', 'content', 'price', 'published')
+    list_display = ('title', 'content', 'price', 'published', 'rubric')
     list_display_links = ('title', 'content')
     search_fields = ('title', 'content',)
 
 
 # Register your models here.
 admin.site.register(Bb, BdAdmin)
+admin.site.register(Rubric)
