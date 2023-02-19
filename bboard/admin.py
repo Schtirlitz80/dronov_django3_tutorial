76a39d15a702c6f343f3ee3f750e64c7c53c5f34
Schtirlitz80 <Schtirlitz80@mail.ru>
2023-02-19 12:08:30 +0300

    Chapter 1 of Dronov's book's done

--- bboard/admin.py
+++ bboard/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Bb
+
+
+class BdAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'price', 'published')
+    list_display_links = ('title', 'content')
+    search_fields = ('title', 'content',)
+
+
 # Register your models here.
+admin.site.register(Bb, BdAdmin)
